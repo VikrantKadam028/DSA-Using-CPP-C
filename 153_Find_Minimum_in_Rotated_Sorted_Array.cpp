bd28baeeb98644b46ff0839c626866ddc440263c
Vikrant Kadam <vikrantkk2889@gmail.com>
2025-10-01 15:20:30 +0530

    Create 153_Find_Minimum_in_Rotated_Sorted_Array.cpp

---
+++ 153_Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,26 @@
+// Brute Force
+// class Solution {
+// public:
+//     int findMin(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         return nums[0];
+//     }
+// };
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+};
