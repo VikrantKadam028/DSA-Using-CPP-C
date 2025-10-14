9c76f98e3c5c50187e1ae247e2744dff631dd7cd
Vikrant Kadam <vikrantkk2889@gmail.com>
2025-10-14 21:17:44 +0530

    Create 367_Valid_Perfect_Square.cpp

---
+++ 367_Valid_Perfect_Square.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int low = 1;
+        int high = num;
+
+        while (low <= high) {
+            long long mid = low + (high-low) / 2;
+            if ((long long)mid * mid == num)
+                return true;
+            else if ((long long)mid * mid < num)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return false;
+    }
+};
