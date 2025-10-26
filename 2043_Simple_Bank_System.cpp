75517d34f9a88b18c235ff91d89f8e1288f1b081
Vikrant Kadam <vikrantkk2889@gmail.com>
2025-10-26 10:48:03 +0530

    Create 2043_Simple_Bank_System.cpp

---
+++ 2043_Simple_Bank_System.cpp
@@ -0,0 +1,40 @@
+class Bank {
+private:
+    vector<long long> balance;
+
+    bool isValid(int account) {
+        return account >= 1 && account <= balance.size();
+    }
+
+public:
+    Bank(vector<long long>& balance) { this->balance = balance; }
+
+    bool transfer(int account1, int account2, long long money) {
+        if (!isValid(account1) || !isValid(account2))
+            return false;
+        if (balance[account1 - 1] < money)
+            return false;
+
+        balance[account1 - 1] -= money;
+        balance[account2 - 1] += money;
+        return true;
+    }
+
+    bool deposit(int account, long long money) {
+        if (!isValid(account))
+            return false;
+
+        balance[account - 1] += money;
+        return true;
+    }
+
+    bool withdraw(int account, long long money) {
+        if (!isValid(account))
+            return false;
+        if (balance[account - 1] < money)
+            return false;
+
+        balance[account - 1] -= money;
+        return true;
+    }
+};
