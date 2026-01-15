770faef8f5886b46869b4a91c2e6ed9e703e3e37
Vikrant Kadam <vikrantkk2889@gmail.com>
2026-01-15 13:08:12 +0530

    Create 142_Linked_List_Cycle_II.cpp

---
+++ 142_Linked_List_Cycle_II.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* detectCycle(ListNode* head) {
+
+        if (head == nullptr)
+            return head;
+        ListNode* temp = head;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        //Apply Floyd’s Cycle Detection 2 times
+        // 1. to detect the cycle
+        // 2. find cycle start
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                slow = head;
+
+                while(slow != fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+       return nullptr; 
+    }
+};
