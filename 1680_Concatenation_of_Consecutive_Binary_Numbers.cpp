49e9685fdcd0bb1b306c9cbf588c72454c7c3877
Vikrant Kadam <vikrantkk2889@gmail.com>
2026-02-28 10:32:40 +0530

    Create 1680_Concatenation_of_Consecutive_Binary_Numbers.cpp

---
+++ 1680_Concatenation_of_Consecutive_Binary_Numbers.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        const int MOD = 1e9 + 7;
+        long long result = 0;
+
+        for(int i = 1; i <= n; i++){
+            int bits = log2(i) + 1;  
+            result = ((result << bits) + i) % MOD;
+        }
+
+        return result;
+    }
+};
