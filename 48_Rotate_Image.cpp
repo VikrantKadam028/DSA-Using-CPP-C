04371be69c1e0f2332deea6bc8338501a847165c
Vikrant Kadam <vikrantkk2889@gmail.com>
2026-06-15 15:41:12 +0530

    Add: Rotate Image

---
+++ 48_Rotate_Image.cpp
@@ -0,0 +1,36 @@
+// BRUTE
+//  class Solution {
+//  public:
+//      void rotate(vector<vector<int>>& matrix) {
+//          int n = matrix.size();
+//          vector<vector<int>> temp(n, vector<int>(n));
+
+//         for(int i = 0; i < n; i++) {
+//             for(int j = 0; j < n; j++) {
+//                 temp[j][n-1-i] = matrix[i][j];
+//             }
+//         }
+
+//         matrix = temp;
+//     }
+// };
+
+// OPTIMAL - matrix->traverse->rev each row
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        // traverse
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
