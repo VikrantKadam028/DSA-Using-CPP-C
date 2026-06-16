bd8a90b0f677bee0b6183eb4f059cb08471475b8
Vikrant Kadam <vikrantkk2889@gmail.com>
2026-06-16 14:17:33 +0530

    Add: Pascals Triangle

---
+++ 118_Pascals_Triangle.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        if (numRows == 0) {
+            return ans;
+        } else if (numRows == 1) {
+            vector<int> result;
+            result.push_back(1);
+            ans.push_back(result);
+            return ans;
+        }
+
+        else if (numRows > 2) {
+            vector<int> row1;
+            row1.push_back(1);
+            ans.push_back(row1);
+
+            vector<int> row2;
+            row2.push_back(1);
+            row2.push_back(1);
+            ans.push_back(row2);
+
+            numRows = numRows - 2;
+        }
+
+        else if (numRows == 2) {
+            vector<int> row1;
+            row1.push_back(1);
+            ans.push_back(row1);
+
+            vector<int> row2;
+            row2.push_back(1);
+            row2.push_back(1);
+            ans.push_back(row2);
+            return ans;
+        }
+        int size = 3;
+        for (int i = 0; i < numRows; i++) {
+
+            vector<int> result(size, 0);
+
+            int left = 0;
+            int right = 1;
+
+            result[0] = 1;
+            result[size - 1] = 1;
+
+            vector<int> prev = ans[size - 2];
+            while (right < prev.size()) {
+                long long sum = prev[left] + prev[right];
+                result[right] = sum;
+
+                left++;
+                right++;
+            }
+            size++;
+            ans.push_back(result);
+        }
+
+        return ans;
+    }
+};
