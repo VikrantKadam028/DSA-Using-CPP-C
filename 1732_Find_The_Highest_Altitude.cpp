8e72336b90c1118bffa3e5352065b2e8e7ca4b26
Vikrant Kadam <vikrantkk2889@gmail.com>
2026-06-19 11:42:26 +0530

    Add: Find The Highest Altitude

---
+++ 1732_Find_The_Highest_Altitude.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int curr = 0;
+        int highest = curr;
+
+        for (int n : gain) {
+            curr += n;
+
+            highest = max(curr, highest);
+        }
+
+        return highest;
+    }
+};
