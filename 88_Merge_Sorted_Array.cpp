2206ae68d994586376fa4a1d5f750d3d91718fe8
Vikrant Kadam <vikrantkk2889@gmail.com>
2026-06-20 12:31:28 +0530

    Update: Merge Sorted Array

--- 88_Merge_Sorted_Array.cpp
+++ 88_Merge_Sorted_Array.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        nums1.resize(m);
-
-        for (int i = 0; i < n; i++) {
-            nums1.push_back(nums2[i]);
+        int left = m ;
+        int right = 0;
+        while(right < n) {
+            nums1[left] = nums2[right];
+            left++;
+            right++;
         }
 
         sort(nums1.begin(), nums1.end());
+      
     }
 };
