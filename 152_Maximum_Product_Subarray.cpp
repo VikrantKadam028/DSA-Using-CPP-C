a30d930df34cc55bd77d5aa316798f3fd07b9ba4
Vikrant Kadam <vikrantkk2889@gmail.com>
2026-06-22 12:42:55 +0530

    Update: Maximum Product Subarray

--- 152_Maximum_Product_Subarray.cpp
+++ 152_Maximum_Product_Subarray.cpp
@@ -1,35 +1,52 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int maxP = INT_MIN;
-        int maxS = INT_MIN;
+        int maxi = INT_MIN;
+        int prefix = 1;
+        int suffix = 1;
+        int n = nums.size();
 
-        int prod = 1;
-
-        // prefix sum
         for (int i = 0; i < nums.size(); i++) {
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
 
-            prod *= nums[i];
-            maxP = max(prod, maxP);
+            prefix *= nums[i];
+            suffix *= nums[n - i - 1];
 
-            if (nums[i] == 0) {
-                prod = 1;
-            }
+            maxi = max(maxi, max(prefix, suffix));
         }
 
-        prod = 1;
+        return maxi;
 
-        // suffix sum
-        for (int i = nums.size() - 1; i >= 0; i--) {
+        // int maxP = INT_MIN;
+        // int maxS = INT_MIN;
 
-            prod *= nums[i];
-            maxS = max(prod, maxS);
+        // int prod = 1;
 
-            if (nums[i] == 0) {
-                prod = 1;
-            }
-        }
+        // // prefix sum
+        // for (int i = 0; i < nums.size(); i++) {
 
-        return max(maxP, maxS);
+        //     prod *= nums[i];
+        //     maxP = max(prod, maxP);
+
+        //     if (nums[i] == 0) {
+        //         prod = 1;
+        //     }
+        // }
+
+        // prod = 1;
+
+        // // suffix sum
+        // for (int i = nums.size() - 1; i >= 0; i--) {
+
+        //     prod *= nums[i];
+        //     maxS = max(prod, maxS);
+
+        //     if (nums[i] == 0) {
+        //         prod = 1;
+        //     }
+        // }
+
+        // return max(maxP, maxS);
     }
 };
