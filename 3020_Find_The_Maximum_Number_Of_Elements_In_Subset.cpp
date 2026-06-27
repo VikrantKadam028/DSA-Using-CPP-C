b0690c004bdec938c3bbdf48934f44f4c9229bcf
Vikrant Kadam <vikrantkk2889@gmail.com>
2026-06-27 12:22:57 +0530

    Add: Find The Maximum Number Of Elements In Subset

---
+++ 3020_Find_The_Maximum_Number_Of_Elements_In_Subset.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        unordered_map<int, int> mp;
+
+        for (int n : nums) {
+            mp[n]++;
+        }
+
+        sort(nums.begin(), nums.end());
+        int maxi = INT_MIN;
+
+        for (auto& it : mp) {
+            int x = it.first;
+
+            if (x == 1) {
+                if (mp[1] % 2 == 0) {
+                    maxi = max(maxi, mp[1] - 1);
+                } else {
+                    maxi = max(maxi, mp[1]);
+                }
+                continue;
+            }
+
+            long long curr = x;
+            int len = 1;
+
+            while (mp[curr] > 1) {
+                if (curr > 1000000000LL)
+                    break;
+                long long nxtSqr = curr * curr;
+
+                if (mp.find(nxtSqr) == mp.end()) {
+                    break;
+                }
+                len += 2;
+                curr = nxtSqr;
+            }
+
+            if (mp[curr] >= 1)
+                maxi = max(maxi, len);
+            else
+                maxi = max(maxi, len - 1);
+        }
+        return maxi;
+    }
+};
