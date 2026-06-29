02b2f8882aa12b997a2ae8f9cf5dee80a0ef67c8
Vikrant Kadam <vikrantkk2889@gmail.com>
2026-06-29 14:06:00 +0530

    Update: Next Greater Element Ii

--- 503_Next_Greater_Element_Ii.cpp
+++ 503_Next_Greater_Element_Ii.cpp
@@ -1,23 +1,46 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-
+        // OPTIMAL
         int n = nums.size();
         vector<int> nge(n);
         stack<int> st;
 
-        for(int i = 2*n-1;i>=0;i--){
-            while(!st.empty() && st.top() <= nums[i % n]){
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            while (!st.empty() && st.top() <= nums[i % n]) {
                 st.pop();
             }
 
-            if(i < n){
-                nge[i] = st.empty()? -1:st.top();
+            if (i < n) {
+                nge[i] = st.empty() ? -1 : st.top();
             }
 
             st.push(nums[i % n]);
         }
 
         return nge;
+
+        // BRUTE
+        //  int n = nums.size();
+        //  vector<int> nge(n, -1);
+        //  int idx = -1;
+
+        // for (int i = 0; i < nums.size(); i++) {
+        //     bool got = false;
+        //     for (int j = i + 1; j <= i + n - 1; j++) {
+        //         idx = j % n;
+
+        //         if (nums[idx] > nums[i]) {
+        //             nge[i] = nums[idx];
+        //             got = true;
+        //             break;
+        //         }
+        //     }
+
+        //     if (!got)
+        //         nge[i] = -1;
+        // }
+
+        // return nge;
     }
 };
