210532c62b6c796b488d58a78ce6540bf503bc21
Vikrant Kadam <vikrantkk2889@gmail.com>
2026-07-01 19:19:40 +0530

    Update: Minimum Distance Between Three Equal Elements I

--- 3740_Minimum_Distance_Between_Three_Equal_Elements_I.cpp
+++ 3740_Minimum_Distance_Between_Three_Equal_Elements_I.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
     int minimumDistance(vector<int>& nums) {
-        int dist = INT_MAX;
+        if (nums.size() < 3)
+            return -1;
+
+        unordered_map<int, vector<int>> mp;
 
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                for (int k = j + 1; k < nums.size(); k++) {
-                    if (nums[i] == nums[j] && nums[j] == nums[k]) {
-                        int sum = abs(i - j) + abs(j - k) + abs(k - i);
-                        dist = min(dist, sum);
-                    }
+            mp[nums[i]].push_back(i);
+        }
+        int dist = INT_MAX;
+        for (auto& it : mp) {
+            vector<int>& temp = it.second;
+
+            if (temp.size() == 3) {
+                int sum = abs(temp[0] - temp[1]) + abs(temp[1] - temp[2]) +
+                          abs(temp[0] - temp[2]);
+                dist = min(dist, sum);
+            } else if (temp.size() > 3) {
+                for (int i = 0; i < temp.size() - 2; i++) {
+                    int j = i + 1;
+                    int k = j + 1;
+
+                    int sum = abs(temp[i] - temp[j]) + abs(temp[j] - temp[k]) + abs(temp[k] - temp[i]);
+                    dist = min(dist, sum);
                 }
             }
         }
