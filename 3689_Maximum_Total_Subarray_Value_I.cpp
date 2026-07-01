281ed4db966ec6586e16fce77162bce99cbcc897
Vikrant Kadam <vikrantkk2889@gmail.com>
2026-07-01 13:24:26 +0530

    Update: Maximum Total Subarray Value I

--- 3689_Maximum_Total_Subarray_Value_I.cpp
+++ 3689_Maximum_Total_Subarray_Value_I.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     long long maxTotalValue(vector<int>& nums, int k) {
-       sort(nums.begin(), nums.end());
-       int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
 
-       long long diff = nums[n - 1] - nums[0];
+        long long diff = nums[n - 1] - nums[0];
 
-       return k * diff;
+        return k * diff * 1LL;
     }
 };
