c0b2ab0037972961974fadd97abb9a11bd7dfb93
Vikrant Kadam <vikrantkk2889@gmail.com>
2026-07-06 19:13:47 +0530

    Update: Find Right Interval

--- 436_Find_Right_Interval.cpp
+++ 436_Find_Right_Interval.cpp
@@ -9,17 +9,15 @@ public:
 
         if (intervals.size() <= 1 && intervals[0][0] != intervals[0][1])
             return {-1};
-        
 
-            vector<int> result(n);
+        vector<int> result(n);
 
         for (int i = 0; i < n; i++) {
             int start = intervals[i][0];
             int end = intervals[i][1];
 
             int miniS = INT_MAX;
-            int idx = -1;
-
+        
             for (int j = 0; j < n; j++) {
                 int nextS = intervals[j][0];
                 int nextE = intervals[j][1];
