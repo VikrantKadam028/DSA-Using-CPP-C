0c533ea5dfaa08f2c2a5ae29aad13c71d17739a8
Vikrant Kadam <vikrantkk2889@gmail.com>
2026-07-19 19:09:14 +0530

    Update: Smallest Subsequence Of Distinct Characters

--- 1081_Smallest_Subsequence_Of_Distinct_Characters.cpp
+++ 1081_Smallest_Subsequence_Of_Distinct_Characters.cpp
@@ -1,3 +1,19 @@
+// Explanation:
+// we take, freq : To check if the s[i] is already visited or not.
+// map : To control the freq of each char in str.
+// stack : keeping the stack in monotonic nature (increasing), push if greater
+// char , else pop till st.top() > currstr.
+
+// We are iterating through s, then do mpp[s[i]]--, if we have already visited
+// that char then skip. then pop the stack will we not maintain the monotonic
+// nature of stack (we pop only if we know that we can get this char afterwards
+// , like its present somewhere next in the str i.e mpp[s[i]] > 0, else consider
+// that str (dont pop)).
+// then we push that char into stack, and mark it as visited.
+// iterate stack and get result and return rev(result);
+
+//  lexicographically smallest subsequence : suppose there is abc & bca, then as (a < b), So abc is the lexicographically smallest subsequence.
+
 class Solution {
 public:
     string smallestSubsequence(string s) {
