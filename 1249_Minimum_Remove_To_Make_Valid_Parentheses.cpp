0ae4cbbd7bd2307dd57d5546e92a9ce8ce46954d
Vikrant Kadam <vikrantkk2889@gmail.com>
2026-07-20 16:10:03 +0530

    Add: Minimum Remove To Make Valid Parentheses

---
+++ 1249_Minimum_Remove_To_Make_Valid_Parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isMatch(char open, char close) { return open == '(' && close == ')'; }
+    string minRemoveToMakeValid(string s) {
+        stack<pair<char, int>> st;
+
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            if (!st.empty() && isMatch(st.top().first, s[i])) {
+                st.pop();
+            } else {
+                if (!isalpha(s[i])) {
+                    st.push({s[i], i});
+                }
+            }
+        }
+
+        while (!st.empty()) {
+            s[st.top().second] = '*';
+            st.pop();
+        }
+
+        string result = "";
+        for (char c : s) {
+            if (c != '*') {
+                result += c;
+            }
+        }
+        return result;
+    }
+};
