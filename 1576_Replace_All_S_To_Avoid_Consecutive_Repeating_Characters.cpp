ed4b287d55a1794b53c72ed25d506d4b77426dd5
Vikrant Kadam <vikrantkk2889@gmail.com>
2026-07-22 14:37:36 +0530

    Add: Replace All S To Avoid Consecutive Repeating Characters

---
+++ 1576_Replace_All_S_To_Avoid_Consecutive_Repeating_Characters.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string modifyString(string s) {
+        unordered_map<int, char> mpp;
+
+        for (int i = 0; i < 26; i++) {
+            mpp[i] = 'a' + i;
+        }
+
+        if (s.length() >= 26) {
+            int idx = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s[i] == '?') {
+                    if (idx > 25) {
+                        idx = 0;
+                    }
+
+                    s[i] = mpp[idx];
+                    idx++;
+                }
+            }
+            return s;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] != '?') {
+                mpp.erase(s[i] - 'a');
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            int idx = 0;
+            if (s[i] == '?') {
+                for (int i = 0; i < 26; i++) {
+                    if (mpp.find(i) != mpp.end()) {
+                        idx = i;
+                        break;
+                    }
+                }
+
+                s[i] = mpp[idx];
+                mpp.erase(idx);
+            }
+        }
+
+        return s;
+    }
+};
