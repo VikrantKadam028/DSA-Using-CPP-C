f3a37deb26506392d5fbfe78eb2a0538ef23e78f
Vikrant Kadam <vikrantkk2889@gmail.com>
2026-07-23 22:17:29 +0530

    Update: Max Consecutive Ones Iii

--- 1004_Max_Consecutive_Ones_Iii.cpp
+++ 1004_Max_Consecutive_Ones_Iii.cpp
@@ -2,6 +2,38 @@ class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
         int n = nums.size();
+        int l = 0;
+        int r = 0;
+        int maxlen = 0;
+        int zeros = 0;
+
+        while (r < n) {
+            if (nums[r] == 0) {
+                zeros++;
+            }
+
+            if (zeros > k) {
+                if (nums[l] == 0) {
+                    zeros--;
+                }
+                l++;
+            }
+
+            if (zeros <= k) {
+                maxlen = max(maxlen, r - l + 1);
+            }
+            r++;
+        }
+        return maxlen;
+    }
+};
+
+//O(2N)
+/*
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int n = nums.size();
         int left = 0;
         int right = 0;
         int zeros = 0;
@@ -25,3 +57,4 @@ public:
         return maxlen;
     }
 };
+*/
