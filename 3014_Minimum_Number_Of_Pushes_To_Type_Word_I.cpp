78d59354f669df48e5fcfe90ec9562761835a69a
Vikrant Kadam <vikrantkk2889@gmail.com>
2026-07-30 18:51:46 +0530

    Update: Minimum Number Of Pushes To Type Word I

--- 3014_Minimum_Number_Of_Pushes_To_Type_Word_I.cpp
+++ 3014_Minimum_Number_Of_Pushes_To_Type_Word_I.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minimumPushes(string word) {
+        //means for first 8 chars push 1, for the next 8 chars push 2 and so on....
        int result = 0;
 
        for(int i=0;i<word.length();i++){
