5e958586dfd5cf3594ef993535bbf8587c3a0b07
Vikrant Kadam <vikrantkk2889@gmail.com>
2026-07-30 18:58:20 +0530

    Update: Count Odd Numbers In An Interval Range

--- 1523_Count_Odd_Numbers_In_An_Interval_Range.cpp
+++ 1523_Count_Odd_Numbers_In_An_Interval_Range.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     int countOdds(int low, int high) {
-        int cnt = 0;
-        for(int i=low; i<= high;i++){
-            if(i % 2 != 0){
-                cnt++;
+        int count=high-low+1;
+        if(count%2==0)
+        {
+            return count/2;
+        }
+        else if(count%2!=0)
+        {
+            if(low%2!=0)
+            {
+                return (count/2)+1;
+            }
+            else
+            {
+                return count/2;
             }
         }
-        return cnt;
+        return count/2;
     }
 };
