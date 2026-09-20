98797f9c9378a7c7869f45e2225636ffdcc810cc
Vikrant Kadam <vikrantkk2889@gmail.com>
2026-09-20 20:46:30 +0530

    Update: Reverse Degree Of A String

--- 3498_Reverse_Degree_Of_A_String.cpp
+++ 3498_Reverse_Degree_Of_A_String.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-        long long sum = 0;
-        long long alphabetpos = 0;
-        long long string_pos = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s[i];
+        int sum = 0;
 
-            alphabetpos = (26 - (c - 'a'));
-            string_pos = i + 1;
+        for(int i = 0; i < s.length(); i++)
+        {
+            int idx = i + 1;
+            char ch = s[i];
+            int val = 26 - (ch - 'a');
 
-            sum += alphabetpos * string_pos;
+            sum += idx * val;
         }
+
         return sum;
     }
 };
